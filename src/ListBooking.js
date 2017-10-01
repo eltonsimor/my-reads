0b2d3bc3309b74c051c72a694cacdc209b9d74fc
Elton Moraes <eltonsimor@gmail.com>
2017-10-01 18:25:32 -0300

    First Project

---
+++ src/ListBooking.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ListBooking extends Component{
+    render(){
+        return(
+            <div>List Booking!!</div>
+        )
+    }
+}
+
+export default ListBooking
