ee3564704ab8c838a59694bd9e6ffcdaf1162105
Elton Moraes <eltonsimor@gmail.com>
2017-10-02 08:43:55 -0300

    include browserrouter into index.js and change ListBooking to LineBooking

---
+++ src/LineBooking.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class LineBooking extends Component{
+    render(){
+        return(
+            <div>Line Booking!!</div>
+        )
+    }
+}
+
+export default LineBooking
