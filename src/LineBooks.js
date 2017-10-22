372ddf20fd3556e2f1fe23df34ba7c1268c30fdf
Elton Moraes <eltonsimor@gmail.com>
2017-10-22 12:11:23 -0200

    altardo o linebooks de react component para fsc

--- src/LineBooks.js
+++ src/LineBooks.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import LineBook from './LineBook'
 
 
-class LineBooks extends Component {
-  render(){
-    const { shelfTitles, updateBook, books } = this.props    
-    return(
-        <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-              {shelfTitles.map((shelf) => (
-                <div key={ shelf.code }>
-                  <LineBook 
-                    shelfTitle={ shelf.title } 
-                    books={ books.filter((book) => book.shelf === shelf.code) } 
-                    updateBook={ updateBook }
-                  />
-                </div>
-              ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link 
-            to="/search">Add a book</Link>
+const LineBooks = (props) => {
+  
+  const { shelfTitles, updateBook, books } = props    
+  return(
+      <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+            {shelfTitles.map((shelf) => (
+              <div key={ shelf.code }>
+                <LineBook 
+                  shelfTitle={ shelf.title } 
+                  books={ books.filter((book) => book.shelf === shelf.code) } 
+                  updateBook={ updateBook }
+                />
+              </div>
+            ))}
         </div>
       </div>
-    )  
-  }   
+      <div className="open-search">
+        <Link 
+          to="/search">Add a book</Link>
+      </div>
+    </div>
+  )  
+  
 }
 
 export default LineBooks
